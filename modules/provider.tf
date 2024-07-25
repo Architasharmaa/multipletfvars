7bdff0102c5af686b07da306e0deeca9e85fb9f4
Archita Sharma <159230390+Architasharmaa@users.noreply.github.com>
2024-07-25 16:02:04 +0530

    second commit

--- modules/provider.tf
+++ modules/provider.tf
@@ -1,11 +1,33 @@
+# terraform {
+#   required_providers {
+#     newrelic = {
+#       source  = "newrelic/newrelic"
+#       version = "3.37.1"
+#     }
+#   }
+# }
+# terraform {
+#   required_providers {
+#     newrelic = {
+#       source  = "newrelic/newrelic"
+#       version = "~> 3.0"   # Update this line
+#     }
+#   }
+# }
 terraform {
   required_providers {
     newrelic = {
-      source  = "newrelic/newrelic"
-      version = "3.38.1"
+      source = "newrelic/newrelic"
+      version = ">= 3.28.3"  
     }
+  
   }
+  
 }
+
+
+
+
 provider "newrelic" {
   account_id = var.account_id
   api_key    = var.api_key
